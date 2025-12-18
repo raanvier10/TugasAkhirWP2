7a64627d8cf5539d938755fa07e6445796ab418e
fauzanms@janojan29 <fauzanms@janojan29>
2025-12-18 09:37:49 +0700

    menambahkancontrolerbarang

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $barangs = Barang::latest()->get();
+        return view('barang.index', compact('barangs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('barang.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode_barang' => 'required|string|max:50|unique:barangs,kode_barang',
+            'nama_barang' => 'required|string|max:255',
+            'satuan' => 'required|string|max:50',
+            'stok_awal' => 'required|integer|min:0',
+        ]);
+
+        Barang::create([
+            'kode_barang' => $request->kode_barang,
+            'nama_barang' => $request->nama_barang,
+            'satuan' => $request->satuan,
+            'stok_awal' => $request->stok_awal,
+            'stok_sekarang' => $request->stok_awal, // stok_sekarang = stok_awal saat pertama dibuat
+        ]);
+
+        return redirect()->route('barang.index')
+            ->with('success', 'Barang berhasil ditambahkan!');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Barang $barang)
+    {
+        return view('barang.edit', compact('barang'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Barang $barang)
+    {
+        $request->validate([
+            'kode_barang' => 'required|string|max:50|unique:barangs,kode_barang,' . $barang->id,
+            'nama_barang' => 'required|string|max:255',
+            'satuan' => 'required|string|max:50',
+        ]);
+
+        $barang->update([
+            'kode_barang' => $request->kode_barang,
+            'nama_barang' => $request->nama_barang,
+            'satuan' => $request->satuan,
+        ]);
+
+        return redirect()->route('barang.index')
+            ->with('success', 'Barang berhasil diupdate!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Barang $barang)
+    {
+        $barang->delete();
+
+        return redirect()->route('barang.index')
+            ->with('success', 'Barang berhasil dihapus!');
+    }
+}
