05bf418bc394b25055471d285f01642012f8b33a
Adam <Muhammad Adam@raanvier10>
2025-12-18 10:46:06 +0700

    update stock keluar

--- app/Http/Controllers/StokKeluarController.php
+++ app/Http/Controllers/StokKeluarController.php
@@ -8,27 +8,18 @@ use Illuminate\Http\Request;
 
 class StokKeluarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $stokKeluars = StokKeluar::with('barang')->latest()->get();
         return view('stok-keluar.index', compact('stokKeluars'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $barangs = Barang::where('stok_sekarang', '>', 0)->orderBy('nama_barang')->get();
         return view('stok-keluar.create', compact('barangs'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $barang = Barang::find($request->barang_id);
@@ -51,9 +42,7 @@ class StokKeluarController extends Controller
             ->with('success', 'Stok keluar berhasil dicatat! Stok ' . $barang->nama_barang . ' berkurang ' . $request->jumlah . ' ' . $barang->satuan);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(StokKeluar $stokKeluar)
     {
         // Tambah kembali stok_sekarang karena data stok keluar dihapus
