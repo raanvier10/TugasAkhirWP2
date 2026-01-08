2ce1622ec6090b26eda315c549d67738ee2c7979
anggapradita <19242052@bsi.ac.id>
2026-01-08 12:49:16 +0700

    memperbaiki RoleMiddleware
    Removed unnecessary PHPDoc comments from RoleMiddleware.

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,12 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     * @param  string  ...$roles
-     */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
         if (!auth()->check()) {
